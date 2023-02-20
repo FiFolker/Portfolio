0d932b8b67620ba23b09ddb59d1d8d6ee4d9c66b
Le Merle Elouan <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-20 20:40:37 +0100

    add animation to arrows and highlighte on hover

--- js/script.js
+++ js/script.js
@@ -5,7 +5,3 @@ function show(id){
         document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
     }
 }
-
-function animeArrow(){
-    
-}
