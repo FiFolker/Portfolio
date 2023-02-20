ac29a92ce7ee04e70bc3b6cd4b626da62a58f367
Elouan Le Merle <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-20 16:51:10 +0100

    add arrows and start of animate Arrow

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ function show(id){
         document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
     }else{
         document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
-
     }
+}
+
+function animeArrow(){
+    
 }
