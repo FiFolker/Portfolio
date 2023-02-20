2a330af6a1f5334835f2f4a9914cd234ac2c0147
Elouan Le Merle <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-20 13:59:03 +0100

    add things

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function show(id){
-    document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` // 
+    if(window.matchMedia("(min-width: 600px)").matches){
+        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
+    }else{
+        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
+
+    }
 }
