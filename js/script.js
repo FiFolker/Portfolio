3afb4ca6a4af21e90ce0493c5a837d7d6b7b3d8e
Elouan Le Merle <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-23 18:01:51 +0100

    add contactMe and add animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
 function show(id){
     if(window.matchMedia("(min-width: 600px)").matches){
-        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
+        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
     }else{
-        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="/ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
+        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
     }
 }
-
-
-/*let content = document.getElementsByClassName("rotate");
-let btn = document.getElementsByClassName("main-section");
-let rot = 0;
-btn.addEventListener("click", function() {
-  content.style = 'transform: rotate(' + rot + 'deg)';
-  rot += 90;
-  console.log(rot)
-});*/
-
