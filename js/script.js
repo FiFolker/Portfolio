e4f2a5f6f9062a045acdf66d49fdd901063038f2
Le Merle Elouan <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-28 23:15:58 +0100

    js for retex is done and functional at 90% i think

--- js/script.js
+++ js/script.js
@@ -1,14 +1,55 @@
+const separator = ";";
+const separatorList = "*";
+const listOfParts = ["cahierDesCharges", "livrable", "methodesEtOutils", "competences"];
+
 function show(id){
-    const thePlaceToBe = document.querySelector("#retexPlace div");
-    if(thePlaceToBe.style.visibility === "hidden"){
-        thePlaceToBe.style.visibility = "visible";
-    }
-    thePlaceToBe.innerHTML=`<p class="text-white">Test du texte qui pourrait être mis là ${id}</p>`;
-    
+	const thePlaceToBe = document.querySelector(".retex"); 
+
+	if(thePlaceToBe.style.visibility === "hidden"){ // rend visible si premier clique car invisible par défaut
+		thePlaceToBe.style.visibility = "visible";
+	}else{
+
+	}
+
+
+	fetch(`/ressources/data/${id}.txt`) // lis le fichier
+	.then(response => {
+		if(response.ok){
+			return response.text();
+		}else{
+			console.log("Erreur dans la réponse ...")
+			return 0;
+		}
+	})
+	.then(content => {
+		if(content == 0){
+			alert(`ERREUR dans le chargement du fichier ${id}`);
+		}else{
+			document.querySelector(".retex h3").textContent = `${capitalizeFirstLetter(id)}`;
+			formatText(content);
+		}
+		
+	})	
+
 }
 
-// if(window.matchMedia("(min-width: 600px)").matches){
-//     document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
-// }else{
-//     document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
-// }
+function formatText(content){
+	const splitedContent = content.split(separator);
+	let listOfContent;
+	let selectedPart;
+	
+	for(let i in splitedContent){ // parcours le contenu splité en 4 
+		selectedPart = document.querySelector(`.${listOfParts[i]} ul`);
+		selectedPart.innerHTML = "";
+		listOfContent = splitedContent[i].split(separatorList);
+		for(let element of listOfContent){ // parcours le contenu splité maintenant en liste
+			selectedPart.innerHTML += `<li>${element}</li>`;
+		}
+		
+	}
+	
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
