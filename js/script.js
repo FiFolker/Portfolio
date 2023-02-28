dbba8ceeea02398baf1755e555b3fa5f30ff116e
Elouan Le Merle <elouan.le-merle@etu.u-bordeaux.fr>
2023-02-28 17:19:12 +0100

    js for show the retex if they are hidden

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function show(id){
-    if(window.matchMedia("(min-width: 600px)").matches){
-        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
-    }else{
-        document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
+    const thePlaceToBe = document.querySelector("#retexPlace div");
+    if(thePlaceToBe.style.visibility === "hidden"){
+        thePlaceToBe.style.visibility = "visible";
     }
+    thePlaceToBe.innerHTML=`<p class="text-white">Test du texte qui pourrait être mis là ${id}</p>`;
+    
 }
+
+// if(window.matchMedia("(min-width: 600px)").matches){
+//     document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}.png" alt="${id}" width="933px" height="500px"/>` 
+// }else{
+//     document.getElementById("retexPlace").innerHTML=`<img class="border border-dark border-4 rounded-3" src="../ressources/retex/${id}Responsive.png" alt="${id}" width="933px" height="500px"/>` 
+// }
