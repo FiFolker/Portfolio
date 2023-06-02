a694909a8b9bc7d8c9e6b11884d871b6689f430b
FiFolker <65813059+FiFolker@users.noreply.github.com>
2023-06-02 15:20:11 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-import data from '../ressources/data/retexData.json' assert {type:'json'};
-
 const separator = ";";
 const separatorList = "*";
 const listOfParts = ["cahierDesCharges", "livrable", "methodesEtOutils", "competences"];
 
-
-function show(id){
+function show(id, page, title){
 	const thePlaceToBe = document.querySelector(".retex"); 
-
-	document.getElementById("temp").textContent = data.programmationAlgorithmique.name;
-
-	fetch(`../ressources/data/${id}.txt`) // lis le fichier
+	fetch(`../ressources/data/${page}/${id}.txt`) // lis le fichier
 	.then(response => {
 		if(response.ok){
 			return response.text();
@@ -26,15 +20,16 @@ function show(id){
 			if(thePlaceToBe.style.visibility === "hidden"){ // rend visible si premier clique car invisible par défaut
 				thePlaceToBe.style.visibility = "visible";
 			}
-			document.querySelector(".retex h3").textContent = `${capitalizeFirstLetter(id)}`;
-			formatText(content, id);
+			document.querySelector(".retex h3").textContent = `${title}`;
+			formatText(content, id, page);
 		}
 		
-	})	
+	})
+
 
 }
 
-function formatText(content, id){
+function formatText(content, id, page){
 	const splitedContent = content.split(separator);
 	let listOfContent;
 	let selectedPart;
@@ -43,7 +38,13 @@ function formatText(content, id){
 		selectedPart = document.querySelector(`.${listOfParts[i]} ul`);
 		selectedPart.innerHTML = "";
 		if(listOfParts[i] === listOfParts[1]){
-			selectedPart.innerHTML += `<img src="../ressources/retex/${id}/">`;
+			listOfContent = splitedContent[i].split(separatorList);
+			for(let element of listOfContent){ // parcours le contenu splité maintenant en liste
+				if(element.includes('.png') || element.includes('.jpg')){
+					selectedPart.innerHTML += `<img src="../ressources/retex/${page}/${id}/${element}" class="m-3 col-3 hoverImage rounded">`;
+					
+				}
+			}
 		}else{
 			listOfContent = splitedContent[i].split(separatorList);
 			for(let element of listOfContent){ // parcours le contenu splité maintenant en liste
@@ -54,18 +55,3 @@ function formatText(content, id){
 	
 }
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function CountFile(folder)
-{
-    // compte le nombre de fichier du repertoire
-    let fileCount = 0;
-    let e = new Enumerator(folder.Files);
-    for ( ; !e.atEnd() ; e.moveNext())
-    {
-        fileCount ++;
-    }
-	return fileCount;
-}
